6b0d2f6588d2a1610ddb45991ed9ab04d8eabacb
Omar Juma <jumao2669@gmail.com>
2023-12-28 15:48:25 +0300

    feat: I have added the functionality for updating auser's job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,35 +1,48 @@
-const Job = require('../models/Job')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require('../errors')
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  const myJob = await Job.find({createdBy: req.user.userId});
-  if(!myJob){
-   throw new NotFoundError("You have not applied for any jobs")
+  const myJob = await Job.find({ createdBy: req.user.userId });
+  if (!myJob) {
+    throw new NotFoundError("You have not applied for any jobs");
   }
-  res.status(StatusCodes.OK).json({myJob, count: myJob.length})
-}
+  res.status(StatusCodes.OK).json({ myJob, count: myJob.length });
+};
 
 const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json(job)
-
-}
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
+};
 
 const updateJob = async (req, res) => {
-  res.send("hi")
+  const {
+    params: { id },
+    body: { position, company },
+    user: { userId },
+  } = req;
 
-}
+  if (position === "" || company === "") {
+    throw new BadRequestError("Company and position can't be empty");
+  }
+  const updatedJob = await Job.findByIdAndUpdate(
+    { _id: id, createdBy: userId },
+    { position, company },
+    { runValidators: true, new: true }
+  );
+  if(!updatedJob){
+    throw new NotFoundError(`Job with id of ${id} was not found`)
+  }
+  res.status(StatusCodes.OK).json(updateJob);
+};
 
 const deleteJob = async (req, res) => {
-  res.send("hi")
-
-}
+  res.send("hi");
+};
 const getJob = async (req, res) => {
-  res.send("hi")
-
-}
+  res.send("hi");
+};
 
 module.exports = {
   createJob,
@@ -37,4 +50,4 @@ module.exports = {
   getAllJobs,
   updateJob,
   getJob,
-}
+};
